df09ccc1de64d4a7f60a47b888c56e5f87d15888
Sanjay Kumar <sanjay.kumar@gemsessence.com>
2020-05-08 06:20:53 +0530

    Sign In and Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "signup" => "users#new"
+  resource :session, only: [:new, :create, :destroy]
   resources :users
 	root "movies#index"
 	resources :movies do
